ea8b49034193c199125c8085415a0b6fa35d9dfc
Travis Nelson <tjnel6691@gmail.com>
2022-10-03 14:05:03 -0400

    Refined dice roll functions and incorporated rolls into Player class.

--- game.js
+++ game.js
@@ -1,54 +1,52 @@
 //Dice Rolls
 
-const rollD4 = (num) => {
+const d4 = (num) => {
   let rollResult = 0
   for (i = 0; i < num; i++) {
     rollResult += Math.ceil(Math.random() * 4)
   }
-  console.log(rollResult)
+  return rollResult
 }
 
-const rollD6 = (num) => {
+const d6 = (num) => {
   let rollResult = 0
   for (i = 0; i < num; i++) {
     rollResult += Math.ceil(Math.random() * 6)
   }
-  console.log(rollResult)
+  return rollResult
 }
 
-const rollD8 = (num) => {
+const d8 = (num) => {
   let rollResult = 0
   for (i = 0; i < num; i++) {
     rollResult += Math.ceil(Math.random() * 8)
   }
-  console.log(rollResult)
+  return rollResult
 }
 
-const rollD10 = (num) => {
+const d10 = (num) => {
   let rollResult = 0
   for (i = 0; i < num; i++) {
     rollResult += Math.ceil(Math.random() * 10)
   }
-  console.log(rollResult)
+  return rollResult
 }
 
-const rollD12 = (num) => {
+const d12 = (num) => {
   let rollResult = 0
   for (i = 0; i < num; i++) {
     rollResult += Math.ceil(Math.random() * 12)
   }
-  console.log(rollResult)
+  return rollResult
 }
 
-const rollD20 = (num) => {
+const d20 = (num) => {
   let rollResult = 0
   for (i = 0; i < num; i++) {
     rollResult += Math.ceil(Math.random() * 20)
   }
-  console.log(rollResult)
+  return rollResult
 }
-
-rollD12(2)
 
 //Objects
 
@@ -56,11 +54,29 @@ class Player {
   constructor(name) {
     this.name = name
     this.currentSpace = 0
-    this.currentRoll
+    this.currentRoll = 0
     this.health = 100
     this.gold = 100
+  }
+  roll(dice, num) {
+    if (dice === d4) {
+      this.currentRoll = d4(num)
+    } else if (dice === d6) {
+      this.currentRoll = d6(num)
+    } else if (dice === d8) {
+      this.currentRoll = d8(num)
+    } else if (dice === d10) {
+      this.currentRoll = d10(num)
+    } else if (dice === d12) {
+      this.currentRoll = d12(num)
+    } else if (dice === d20) {
+      this.currentRoll = d20(num)
+    }
+    console.log(`${this.name} rolled a ${this.currentRoll}`)
   }
 }
 
 const player1 = new Player(`Steve`)
-console.log(player1)
+console.log(`${player1.name}'s current roll is ${player1.currentRoll}.`)
+player1.roll(d10, 3)
+console.log(`${player1.name}'s current roll is ${player1.currentRoll}.`)
