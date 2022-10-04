dc85510191f20ed37d5461861b47fdfdceabed2c
Travis Nelson <tjnel6691@gmail.com>
2022-10-04 10:14:39 -0400

    Streamlined dice roll function.

--- game.js
+++ game.js
@@ -4,57 +4,6 @@ let currentPlayerTurn = 1
 const player1Name = `Zazu`
 const player2Name = `Gumby`
 const button = document.querySelector(`button`)
-// let spaceArr = []
-
-// Dice Rolls
-
-const d4 = (num) => {
-  let rollResult = 0
-  for (i = 0; i < num; i++) {
-    rollResult += Math.ceil(Math.random() * 4)
-  }
-  return rollResult
-}
-
-const d6 = (num) => {
-  let rollResult = 0
-  for (i = 0; i < num; i++) {
-    rollResult += Math.ceil(Math.random() * 6)
-  }
-  return rollResult
-}
-
-const d8 = (num) => {
-  let rollResult = 0
-  for (i = 0; i < num; i++) {
-    rollResult += Math.ceil(Math.random() * 8)
-  }
-  return rollResult
-}
-
-const d10 = (num) => {
-  let rollResult = 0
-  for (i = 0; i < num; i++) {
-    rollResult += Math.ceil(Math.random() * 10)
-  }
-  return rollResult
-}
-
-const d12 = (num) => {
-  let rollResult = 0
-  for (i = 0; i < num; i++) {
-    rollResult += Math.ceil(Math.random() * 12)
-  }
-  return rollResult
-}
-
-const d20 = (num) => {
-  let rollResult = 0
-  for (i = 0; i < num; i++) {
-    rollResult += Math.ceil(Math.random() * 20)
-  }
-  return rollResult
-}
 
 // Objects/Classes
 
@@ -71,18 +20,9 @@ class Player {
     this.inventory = []
   }
   roll(dice, num) {
-    if (dice === d4) {
-      this.currentRoll = d4(num)
-    } else if (dice === d6) {
-      this.currentRoll = d6(num)
-    } else if (dice === d8) {
-      this.currentRoll = d8(num)
-    } else if (dice === d10) {
-      this.currentRoll = d10(num)
-    } else if (dice === d12) {
-      this.currentRoll = d12(num)
-    } else if (dice === d20) {
-      this.currentRoll = d20(num)
+    this.currentRoll = 0
+    for (let i = 0; i < num; i++) {
+      this.currentRoll += Math.ceil(Math.random() * dice)
     }
     console.log(`${this.name} rolled a ${this.currentRoll}`)
   }
@@ -109,9 +49,9 @@ const startTurn = () => {
 
 const rollToMove = (player) => {
   if (player.extraRoll === true) {
-    player.roll(d10, 2)
+    player.roll(10, 2)
   } else {
-    player.roll(d4, 1)
+    player.roll(4, 1)
   }
   player.spaceDiv.classList.remove(`${player.divClass}`)
   player.currentSpace += player.currentRoll
