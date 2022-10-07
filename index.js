7ff162fdf88c8162c0ef90cd6da04f6af7b86717
Travis Nelson <tjnel6691@gmail.com>
2022-10-07 00:22:30 -0400

    More button styling

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ const playGame = document.getElementById(`playGame`)
 playGame.addEventListener(`click`, () => {
   window.location.href = `./game.html`
 })
-
-playGame.addEventListener(`mouseover`, () => {
-  playGame.classList.add(`rainbow`)
-})
